c6f95101a1e093d137c503a284ec4ba0e897eb8b
Michal Rolinek <mrolinek@diesse.is.localnet>
2018-02-20 14:16:45 +0100

    pip setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='L4',
+      version='1.0',
+      description='TensorFlow implementation of L4 stepsize adaptation',
+      url='https://github.com/martius-lab/l4-optimizer',
+      author='Michal Rolinek, MPI-IS Tuebingen, Autonomous Learning',
+      author_email='michalrolinek@gmail.com',
+      license='MIT',
+      scripts=['L4'],
+      install_requires=[],
+      zip_safe=False)
